734fe8e01fd1aab17dd8f3271afaff6271bf0cfe
christianbollacker <christianbollacker@hotmail.com>
2015-02-18 23:12:11 -0800

    Working in jQuery and added site functionality

--- javascript/horserace.js
+++ javascript/horserace.js
@@ -1,4 +1,19 @@
+$(function() {
+  $('.pony button').click(function() {
+    $(this).addClass('chosen');
+    $('.pony button').hide();
+  });
+  $('#info').click(function() {
+    $('footer').toggleClass('footshow');
+  });
 
+});
+
+$('#start').on('click', function() {
+
+});
+
+/*
 var finishLine = 100;
 var horseRace = function() {
   calChrome.run();
@@ -9,6 +24,12 @@ var raceStatus = function() {
   console.log(calChrome.horseStatus());
   console.log(seaBiscuit.horseStatus());
   console.log(seattleSlew.horseStatus());
+}
+
+var allHorseMoves = function() {
+  console.log(calChrome.allMoves());
+  console.log(seaBiscuit.allMoves());
+  console.log(seattleSlew.allMoves());
 }
 var raceWinner = function() {
   if (calChrome.position > seaBiscuit.position && calChrome.position > seattleSlew.position) {
@@ -26,6 +47,7 @@ var Horse = function(name, speed, endurance){
   this.speed = speed;
   this.endurance = endurance;
   this.position = 0;
+  this.eachMove = [0];
   this.raceMojo = function() {
     return(Math.random() + 1);
   };
@@ -37,6 +59,10 @@ var Horse = function(name, speed, endurance){
   };
   this.run = function() {
     this.position += (this.raceSpeed() * this.raceMojo());
+    this.eachMove.push(Math.floor(this.position));
+  };
+  this.allMoves = function() {
+    return('Here are the postions after each move that ' + this.name + " made. " + this.eachMove + " " + this.eachMove.length);
   };
 }
 
@@ -44,26 +70,6 @@ var calChrome = new Horse('California Chrome', 9, 6);
 var seaBiscuit = new Horse('Seabiscuit', 7, 7);
 var seattleSlew = new Horse('Seattle Slew', 5, 8.5);
 
-while(calChrome.position < (finishLine *.25) && seaBiscuit.position < (finishLine * .25) && seattleSlew.position < (finishLine * .25) ) {
-  horseRace();
-};
-
-console.log('We are about ' + (finishLine * .25) + ' yards or 1/4 through the race.  Here are the standings.')
-raceStatus();
-
-while(calChrome.position < (finishLine *.5) && seaBiscuit.position < (finishLine * .5) && seattleSlew.position < (finishLine * .5) ) {
-  horseRace();
-};
-
-console.log('We are about ' + (finishLine * .5) + ' yards or 1/2 through the race.  Here are the standings.')
-raceStatus();
-
-while(calChrome.position < (finishLine *.75) && seaBiscuit.position < (finishLine * .75) && seattleSlew.position < (finishLine * .75)) {
-  horseRace();
-};
-
-console.log('We are about ' + (finishLine * .75) + ' yards or 3/4 through the race.  Here are the standings.')
-raceStatus();
 
 while(calChrome.position < finishLine && seaBiscuit.position < finishLine && seattleSlew.position < finishLine ) {
   horseRace();
@@ -72,3 +78,6 @@ while(calChrome.position < finishLine && seaBiscuit.position < finishLine && sea
 console.log('The race is finished. Here are the standings.')
 raceStatus();
 console.log(raceWinner());
+allHorseMoves();
+
+*/
