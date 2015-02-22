5b4c489eda3dd7e89ad39bb68d044fd2853e170a
christianbollacker <christianbollacker@hotmail.com>
2015-02-21 22:20:35 -0800

    Saturday night - Almost full functionality

--- javascript/horserace.js
+++ javascript/horserace.js
@@ -2,23 +2,35 @@ $(function() {
   $('#start').hide();
   $('#awards').hide();
 
-  $('.pony button').click(function() {
+  $('.pony button').on('click', function() {
     $(this).parent().addClass('chosen');
     $('.pony button').hide();
     $('#start').slideDown('slow');
-    
-    /*This is where I need to find a way to declare my horse choice based on the '.pony button' parent
-     - also make sure the variable has global scope for later use outside this function*/
-    
   });
-  $('#info').click(function() {
+
+  $('#calChrome').on('click', function() {
+    var userChoice = calChrome.name;
+    console.log('The user has picked ' + userChoice + ' to win.');
+  });
+
+  $('#seaBiscuit').on('click', function() {
+    var userChoice = seaBiscuit.name;
+    console.log('The user has picked ' + userChoice + ' to win.');
+  });
+
+  $('#seattleSlew').on('click', function() {
+    var userChoice = seattleSlew.name;
+    console.log('The user has picked ' + userChoice + ' to win.');
+  });
+
+  $('#info').on('click', function() {
     $('footer').toggleClass('footshow');
   });
-});
 
-$('#start').on('click', function() {
 
   var finishLine = 100;
+
+  var userChoice = 0;
 
   var horseRace = function() {
     calChrome.run();
@@ -34,6 +46,12 @@ $('#start').on('click', function() {
     calChrome.raceFinish();
     seaBiscuit.raceFinish();
     seattleSlew.raceFinish();
+  }
+
+  var raceReset = function () {
+    calChrome.horseReset();
+    seaBiscuit.horseReset();
+    seattleSlew.horseReset();
   }
 
   var raceStatus = function() {
@@ -58,6 +76,21 @@ $('#start').on('click', function() {
     else {
       return(seattleSlew.name);
     }
+  }
+
+  function awardShow() {
+    var primaPony = function() {
+      return "<h1> The winner is " + raceWinner(); + "</h1>";
+    } 
+    var goodPick = function () {
+      if (userChoice == raceWinner()) {
+        return ("<p>Your horse won!  You sure now how to pick your ponies.</p>");
+      } else {
+        return ("<p>Your horse lost...  Better luck next time picking the ponies</p>");
+      }
+    }
+    $('#awards').prepend(goodPick());
+    $('#awards').prepend(primaPony());  
   }
 
   function raceHistory() {
@@ -95,6 +128,11 @@ $('#start').on('click', function() {
         $('#' + this.num).animate({marginLeft: '1000'}, 200, 'linear');
       }
     };
+    this.horseReset = function() {
+      this.position = 0;
+      this.eachMove = [0];
+      $('#' + this.num).animate({marginLeft: '0'}, 0);
+    };
     this.allMoves = function() {
       return('Here are the postions after each move that ' + this.name + " made. " + this.eachMove + " " + this.eachMove.length);
     };
@@ -102,30 +140,36 @@ $('#start').on('click', function() {
 
   var calChrome = new Horse('California Chrome', 9, 6, 'horse1');
   var seaBiscuit = new Horse('Seabiscuit', 7, 7, 'horse2');
-  var seattleSlew = new Horse('Seattle Slew', 5, 8.5, 'horse3');
+  var seattleSlew = new Horse('Seattle Slew', 5, 8.4, 'horse3');
 
-  while(calChrome.position < finishLine && seaBiscuit.position < finishLine && seattleSlew.position < finishLine ) {
-    horseRace();
-  };
+  $('#start').on('click', function() {
+    while(calChrome.position < finishLine && seaBiscuit.position < finishLine && seattleSlew.position < finishLine ) {
+      horseRace();
+    };
 
-  allFinish();
+    allFinish();
 
-  console.log('The race is finished. Here are the standings.')
-  raceStatus();
-  console.log(raceWinner());
-  allHorseMoves();
-  $('#awards').show();
+    console.log('The race is finished. Here are the standings.')
+    raceStatus();
+    console.log(raceWinner());
+    allHorseMoves();
+    awardShow();
+
+    $('#awards').delay(4000).show('fast');
+  });
+
+  $('#reset').click(function() {
+    $('#start').hide();
+    $('#awards').hide();
+    $('#awards h1').remove();
+    $('#awards p').remove();
+    $('.pony button').show();
+    $('.pony').removeClass('chosen');
+    raceHistory();
+    raceReset();
+  });
 
 });
-
-$('#reset').click(function() {
-  $('#start').hide();
-  $('#awards').hide();
-  $('.pony button').show();
-  $('.pony').removeClass('chosen');
-  raceHistory();
-});
-
 
 
 
